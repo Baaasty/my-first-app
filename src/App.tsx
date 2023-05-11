fa13551d506c1063a1f5f7d28724a163681cf42c
Baaasty <46427883+Baaasty@users.noreply.github.com>
2023-05-12 01:20:02 +0200

    For each

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import type { Component } from "solid-js";
+import { Component, For, createSignal } from "solid-js";
 
 const App: Component = () => {
-  return <div>
-    <h1>test</h1>
-  </div>;
+  const [cats, setCats] = createSignal([
+    { id: "J---aiyznGQ", name: "Keyboard Cat" },
+    { id: "z_AbfPXTKms", name: "Maru" },
+    { id: "OUtn3pvWmpg", name: "Henri The Existential Cat" },
+  ]);
+
+  return (
+    <ul>
+      <For each={cats()}>
+        {(cat, i) => (
+          <li>
+            <a target="_blank" href={`https://youtube.com/watch?v=${cat.id}`}>
+              {i() + 1}: {cat.name}
+            </a>
+          </li>
+        )}
+      </For>
+    </ul>
+  );
 };
 
 export default App;
